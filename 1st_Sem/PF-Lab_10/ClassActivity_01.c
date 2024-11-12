d78a17acb30ec4d0818e89653bc3837db4e80fd9
Muhammad Usman Pirzada <usman.24231@gmail.com>
2024-11-12 14:20:34 +0500

    Remember to chk 1st

--- 1st_Sem/PF-Lab_10/ClassActivity_01.c
+++ 1st_Sem/PF-Lab_10/ClassActivity_01.c
@@ -1,38 +1,18 @@
 #include <stdio.h>
-
-struct Chocolate{
-	char Name[15];
-	float Weight;
-	int Calories;
-	float Price;
-	char ExpiryDate[15];
-};
-
+int add(int n);
 int main() {
-	struct Chocolate myFavChocolates[10];
-	
-	for(int i = 0; i < 2; i++) {
-		printf("Enter Chocolate Name: ");
-		scanf("%s", myFavChocolates[i].Name);
-		printf("Enter Chocolate Weight: ");
-		scanf("%f", &myFavChocolates[i].Weight);
-		printf("Enter Chocolate Calories: ");
-		scanf("%d", &myFavChocolates[i].Calories);
-		printf("Enter Chocolate Price: ");
-		scanf("%f", &myFavChocolates[i].Price);
-		printf("Enter Chocolate Expiry Date: ");
-		scanf("%s", myFavChocolates[i].ExpiryDate);
-		printf("\n");
-	}	
-	for(int i = 0; i < 2; i++) {
-		printf("\nChocolate %d:\n", i + 1);
-		printf("Name: %s\n", myFavChocolates[i].Name);
-		printf("Weight: %.2f\n", myFavChocolates[i].Weight);
-		printf("Calories: %d\n", myFavChocolates[i].Calories);
-		printf("Price: $%.2f\n", myFavChocolates[i].Price);
-		printf("Expiry Date %s\n", myFavChocolates[i].ExpiryDate);
-		printf("\n");
-	}
-	
+	int num;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	printf("\nThe sum of natural numbers from \'0\' to \'%d\' is: %d", num, add(num));
 	return 0;
+}
+
+int add(int n) {
+	int sum = 0;
+	if(n == 0){
+		return n;
+	} else if(n > 0) {
+		sum = n + add(n-1);
+	}
 }
