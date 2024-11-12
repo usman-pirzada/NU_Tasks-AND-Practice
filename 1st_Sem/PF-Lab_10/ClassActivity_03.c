f208ae0b7db6d3d107ed8531eefb5cf4085cee27
Muhammad Usman Pirzada <usman.24231@gmail.com>
2024-11-12 15:04:06 +0500

    .

--- 1st_Sem/PF-Lab_10/ClassActivity_03.c
+++ 1st_Sem/PF-Lab_10/ClassActivity_03.c
@@ -5,16 +5,25 @@ int main() {
     printf("How many numbers do you want to enter in the array?: ");
     scanf("%d", &n);
     int arr[n];
-
+    printf("\n");
     for(int i = 0; i < n; i++) {
-        for(int j = n; j > 0; j--) {
-            int temp;
+        printf("Enter Number#%d in the Array:", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
             if(arr[j] > arr[j + 1]) {
-                temp = arr[j];
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
+    }
+
+    printf("\nHere is sorted one:\n");
+    for(int i = 0; i < n; i++) {
+        printf("  %d", arr[i]);
     }
     return 0;
 }
