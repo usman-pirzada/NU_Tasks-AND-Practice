40e3837d892dc3384ac2d0e9ec2800e145eb23a3
Muhammad Usman Pirzada <usman.24231@gmail.com>
2024-11-12 15:55:56 +0500

    .

--- 1st_Sem/PF-Lab_10/24k-3000_Q#01.c
+++ 1st_Sem/PF-Lab_10/24k-3000_Q#01.c
@@ -10,7 +10,7 @@ struct Travel_Package {
 
 int main() {
     int n;
-    printf("How much package\'s data do you want to store: ");
+    printf("How much Travel Packages do you want to store: ");
     scanf("%d", &n);
 
     struct Travel_Package pkg[n];
@@ -19,11 +19,11 @@ int main() {
         printf("\nPackage#%d\n", i + 1);
         printf("Enter Package Name: ");
         scanf("%s", &pkg[i].name);
-        printf("Enter its Destination: ");
+        printf("Enter Destination: ");
         scanf("%s", &pkg[i].dest);
-        printf("Enter its delievery duration in hours: ");
+        printf("Enter Duration in days: ");
         scanf("%d", &pkg[i].duration);
-        printf("Enter its Cost: ");
+        printf("Enter Cost: ");
         scanf("%f", &pkg[i].cost);
         printf("Enter no. of seats available: ");
         scanf("%d", &pkg[i].seats);
@@ -33,7 +33,7 @@ int main() {
         printf("\n\nPackage#%d", i + 1);
         printf("\nNamePackage: %s", pkg[i].name);
         printf("\nDestination: %s", pkg[i].dest);
-        printf("\nDelievery Duration: %d Hrs", pkg[i].duration);
+        printf("\nDuration: %d Days", pkg[i].duration);
         printf("\nCost: %.2f", pkg[i].cost);
         printf("\nSeats Available: %d", pkg[i].seats);
     }
