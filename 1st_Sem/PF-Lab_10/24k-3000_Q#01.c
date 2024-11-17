d7b62d44742646eaecb539b845c1a6b9cae7bfe9
usman-pirzada <k243000@nu.edu.pk>
2024-11-17 22:48:09 +0500

    .

--- 1st_Sem/PF-Lab_10/24k-3000_Q#01.c
+++ 1st_Sem/PF-Lab_10/24k-3000_Q#01.c
@@ -1,42 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-struct Travel_Package {
-    char name[20];
-    char dest[30];
-    int duration;
-    float cost;
-    int seats;
-};
+int vowelCount(char[], int);
 
 int main() {
-    int n;
-    printf("How much Travel Packages do you want to store: ");
-    scanf("%d", &n);
+    char str[25];
+    int vowels;
 
-    struct Travel_Package pkg[n];
-
-    for(int i = 0; i < n; i++) {
-        printf("\nPackage#%d\n", i + 1);
-        printf("Enter Package Name: ");
-        scanf("%s", &pkg[i].name);
-        printf("Enter Destination: ");
-        scanf("%s", &pkg[i].dest);
-        printf("Enter Duration in days: ");
-        scanf("%d", &pkg[i].duration);
-        printf("Enter Cost: ");
-        scanf("%f", &pkg[i].cost);
-        printf("Enter no. of seats available: ");
-        scanf("%d", &pkg[i].seats);
-    }
-    
-    for(int i = 0; i < n; i++) {
-        printf("\n\nPackage#%d", i + 1);
-        printf("\nNamePackage: %s", pkg[i].name);
-        printf("\nDestination: %s", pkg[i].dest);
-        printf("\nDuration: %d Days", pkg[i].duration);
-        printf("\nCost: %.2f", pkg[i].cost);
-        printf("\nSeats Available: %d", pkg[i].seats);
-    }
-
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    vowels = vowelCount(str, 0);
+    printf("\nThere are %d vowels in the given string.", vowels);
     return 0;
+}
+
+int vowelCount(char str[], int i) {
+    char ch;
+
+    if(str[i] == '\0') {
+        return 0;
+    }
+
+    ch = tolower(str[i]);   // Uppercase to lower case conversion
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        return 1 + vowelCount(str, i + 1);
+    } else {
+        return vowelCount(str, i + 1);
+    }
 }
