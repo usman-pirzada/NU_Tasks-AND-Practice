7cb6f8d090de2391a0fbe78ec90c1c1f40c034a5
usman-pirzada <k243000@nu.edu.pk>
2024-11-19 00:55:31 +0500

    .

--- 1st_Sem/PF-Theory_Assignment-03/01.c
+++ 1st_Sem/PF-Theory_Assignment-03/01.c
@@ -1,29 +1,59 @@
 #include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+#define n 4	// As there are four customers
+
+// ------------------------------------------
+struct Date {
+	int date;
+	int month;
+	int year;
+} dateOfJoining, currentDate;
 
 struct Employee {
-	char employeeCode[16];
-	char employeeName[21];
-	char dateOfJoining[16];
-} emp[4];
+	char employeeCode[15];
+	char employeeName[20];
+	struct Date dateOfJoining;
+} emp[n];
 
-void assign() {
-	for(int i = 0; i < 4; i++) {
-		printf("\n\n\tEmployee #0%d\n", i + 1);
-		printf("Enter employee\'s code: ");
-		fgets(emp[].employeeCode, sizeof(emp[].employeeCode), stdin);
-		printf("Enter employee\'s name: ");
-		fgets(emp[].employeeName, sizeof(emp[].employeeName), stdin);
-		printf("Enter employee\'s date (DD/MM/YY) of joining: ");
-		fgets(emp[].dateOfJoining, sizeof(emp.dateOfJoining), stdin);
-		tenure();
-}
-
+// -------------------------------------------
 void tenure() {
 	char ch;
+	int count = 0;	// no. of employees with more than 3 years tenure
+	
+	printf("\nEnter the current date in this format (DD MM YYYY): ");
+	scanf("%d %d %d", &currentDate.date, &currentDate.month, &currentDate.year);
+	
+	for(int i = 0; i < n; i++) {
+		if(emp[i].dateOfJoining.year < (currentDate.year - 3)) {
+			printf("\n%s has a tenure of more than three years.", emp[n].employeeName);
+			count++;
+		} else if(emp[i].dateOfJoining.year == (currentDate.year - 3) && emp[i].dateOfJoining.month < currentDate.month) {
+			printf("\n%s has a tenure of more than three years.", emp[n].employeeName);
+			count++;
+		} else if((emp[i].dateOfJoining.year == (currentDate.year - 3)) && (emp[i].dateOfJoining.month == currentDate.month) && (emp[i].dateOfJoining.date <= currentDate.date)) {
+			printf("\n%s has a tenure of more than three years.", emp[n].employeeName);
+			count++;
+		}
+	}
 	
 	printf("\nPress any character to exit.");
 	ch = getchar();
-	exit();
+	exit(0);
+}
+
+void assign() {
+	for(int i = 0; i < n; i++) {
+		printf("\n\n\tEmployee #0%d\n", i + 1);
+		printf("Enter employee\'s code: ");
+		fgets(emp[i].employeeCode, sizeof(emp[n].employeeCode), stdin);
+		printf("Enter employee\'s name: ");
+		fgets(emp[i].employeeName, sizeof(emp[n].employeeName), stdin);
+		printf("Enter employee\'s date of joining as (DD MM YYYY): ");
+		scanf("%d %d %d", &emp[i].dateOfJoining.date, &emp[i].dateOfJoining.month, &emp[i].dateOfJoining.year);
+		tenure();
+	}
 }
 
 int main() {
