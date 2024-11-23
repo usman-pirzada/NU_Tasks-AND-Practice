425580d76ad11ef2adeca26219b56226ddea391b
usman-pirzada <k243000@nu.edu.pk>
2024-11-24 01:49:35 +0500

    .

--- 1st_Sem/PF-Lab_11/24k-3000_Q#03.c
+++ 1st_Sem/PF-Lab_11/24k-3000_Q#03.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
 int main() {
+	char buffer;
+	FILE *fptr1, *fptr2;
+	
+	fptr1 = fopen("firstfile.txt", "r");
+	fptr2 = fopen("secondfile.txt", "a");
+	
+	fprintf(fptr2, "\n");
+	while(buffer != EOF) {
+		fgets(buffer, 1000, fptr1);
+		fputs(buffer, fptr2);
+	}
+	fclose(fptr1);
+	
+	fptr1 = fopen("mrgFile.txt", "w");
+	while(buffer != EOF) {
+		fgets(buffer, 100, fptr2);
+		fputs(buffer, fptr1);
+	}
+	
+	fclose(fptr1);
+	fclose(fptr2);
 	return 0;
 }
