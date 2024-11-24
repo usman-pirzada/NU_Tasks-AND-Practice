2b4d9e7154e0e638f8fb049affa907760e3317b1
usman-pirzada <k243000@nu.edu.pk>
2024-11-24 10:55:08 +0500

    Update 24k-3000_Q#03.c

--- 1st_Sem/PF-Lab_11/24k-3000_Q#03.c
+++ 1st_Sem/PF-Lab_11/24k-3000_Q#03.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
+#include <error.h>
 
 int main() {
-	char buffer;
+	char buffer[1000];
 	FILE *fptr1, *fptr2;
 	
-	fptr1 = fopen("firstfile.txt", "r");
-	fptr2 = fopen("secondfile.txt", "a");
+	fptr1 = fopen("firstFile.txt", "r");
+	if(fptr1 == NULL) {
+		perror("Error (firstFile.txt)");
+		fptr1 = fopen("firstFile.txt", "a");
+	}
+	fptr2 = fopen("secondFile.txt", "a");
+	if(fptr2 == NULL) {
+		perror("Error (secondFile.txt)");
+		return 0;
+	}
 	
 	fprintf(fptr2, "\n");
-	while(buffer != EOF) {
+	while(buffer != '\n') {
 		fgets(buffer, 1000, fptr1);
 		fputs(buffer, fptr2);
 	}
 	fclose(fptr1);
 	
 	fptr1 = fopen("mrgFile.txt", "w");
-	while(buffer != EOF) {
+	if(fptr1 == NULL) {
+		perror("Error (mrgFile.txt)");
+		return 0;
+	}
+	while(buffer != '\n') {
 		fgets(buffer, 100, fptr2);
 		fputs(buffer, fptr1);
 	}
