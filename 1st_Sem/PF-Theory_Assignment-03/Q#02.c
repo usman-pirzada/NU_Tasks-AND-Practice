f3b5b7ef8f224925d09b44ab9a08f072ac729234
usman-pirzada <k243000@nu.edu.pk>
2024-11-26 17:23:20 +0500

    .

---
+++ 1st_Sem/PF-Theory_Assignment-03/Q#02.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct Player {
+	char playerName;
+	unsigned int ballScores;
+	unsigned int totalScore;
+};
+
+int main() {
+	
+	
+	return 0;
+}
+
+playerGame() {
+	
+}
+
+validateScore() {
+	
+}
+
+findWinner() {
+	
+}
+
+scoreBoard() {
+	
+}
