61d5ac1f0a8f37127d45a6694a058059230e689b
Muhammad Usman Pirzada <usman.24231@gmail.com>
2024-11-27 23:17:04 +0500

    .

--- 1st_Sem/PF-Theory_Assignment-03/Q#02.c
+++ 1st_Sem/PF-Theory_Assignment-03/Q#02.c
@@ -8,36 +8,37 @@ struct Player {
 	unsigned int totalScore;
 };
 
-void playGame(struct, int*);
-bool validateScore(int *);
-void scoreBoard(struct);
-findWinner();
+void playGame(struct Player, int*, int*);
+int validateScore(int*);
+void scoreBoard(struct Player);
+void findWinner(struct Player);
 
 int main() {
 	struct Player players[totPlyr];
-    for(int plyr = 0, plyr < totPlyr; plyr++) {    // Iterate for two players
+    for(int plyr = 0; plyr < totPlyr; plyr++) {    // Iterate for two players
         for(int ball = 0; ball < 12; ball++) {  // Iterate for 12 balls
-            playGame(players, plyr, ball);
+            playGame(*players, &plyr, &ball);
         }
     }
-	
-	scoreBoard(players);
+
+	scoreBoard(*players);
+	findWinner(*players);
 	
 	return 0;
 }
 
-void playGame(struct Player *players, int *plyr, int *ball) {
+void playGame(struct Player players, int *plyr, int *ball) {
 	int currentScore = 0;
 
     printf("Enter your score for ball#%d: ", ball + 1);
 	scanf("%d", &currentScore);
-	if(validateScore(currentScore)) {
+	if(validateScore(&currentScore)) {
 		players[plyr]->ballScores[ball] = currentScore;
 		players[plyr]->totalScore += currentScore;
 	}
 }
 
-bool validateScore(int *currentScore) {
+int validateScore(int *currentScore) {
     if(currentScore >= 0 && currentScore <= 6) {
         return 1;
     } else {
@@ -51,10 +52,14 @@ void scoreBoard(struct Player *players) {
 		for(int j = 0; j < 12; j++) {
 			printf(" Ball#%d: %d\n", j + 1, players->ballScores[j]);
 		}
-		printf("\n\tTotal Scores: %d", players->totalScore);
+		printf("\n\tTotal Scores: %d", players[i]->totalScore);
 	}
 }
 
-findWinner() {
-	
+findWinner(struct Player *players) {
+	if(players[0]->totalScore > players[1]->totalScore) {
+		printf("\n\n\t\t%s id the winner!", players->playerName)
+	} else if(players[0]->totalScore == players[1]->totalScore) {
+
+	}
 }
