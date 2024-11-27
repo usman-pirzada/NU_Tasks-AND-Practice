4cd1e9dd6b6b5bd0ccc1eb09db476b1cc69cf8b9
Muhammad Usman Pirzada <usman.24231@gmail.com>
2024-11-27 18:54:39 +0500

    .

--- 1st_Sem/PF-Theory_Assignment-03/Q#02.c
+++ 1st_Sem/PF-Theory_Assignment-03/Q#02.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Player {
-	char playerName;
+	char playerName[20];
 	unsigned int ballScores;
 	unsigned int totalScore;
 };
 
 int main() {
-	
+	struct Player players[p];
+    for(int i = 0, i < p; i++) {    // Iterate for two players
+        for(int balls = 1; balls <= 12; balls++) {  // Iterate for 12 balls
+            playGame(players[i], i);
+        }
+    }
 	
 	return 0;
 }
 
-playerGame() {
-	
+void playGame(struct Player *players, int *i) {
+    printf("Enter your score for ball#%d: ", ball);
+    scanf("%d", &players[i]->ballScores);
+    validateScore(score);
 }
 
-validateScore() {
-	
+validateScore(int *score) {
+    if() {
+        return;
+    } else {
+
+    }
 }
 
 findWinner() {
