0731d74ce2fb9296b38283a1c0fbdcdec13526cc
Muhammad Usman Pirzada <usman.24231@gmail.com>
2024-11-27 21:28:42 +0500

    .

--- 1st_Sem/PF-Theory_Assignment-03/Q#02.c
+++ 1st_Sem/PF-Theory_Assignment-03/Q#02.c
@@ -2,35 +2,38 @@
 #include <stdlib.h>
 #define totPlyr 2	// For two players
 
-void playGame(struct, int*);
-bool validateScore(int *);
-findWinner();
-scoreBoard()
-
 struct Player {
 	char playerName[20];
 	unsigned int ballScores[12];
 	unsigned int totalScore;
 };
 
+void playGame(struct, int*);
+bool validateScore(int *);
+void scoreBoard(struct);
+findWinner();
+
 int main() {
 	struct Player players[totPlyr];
     for(int plyr = 0, plyr < totPlyr; plyr++) {    // Iterate for two players
         for(int ball = 0; ball < 12; ball++) {  // Iterate for 12 balls
-            playGame(players[plyr], plyr, ball);
+            playGame(players, plyr, ball);
         }
     }
+	
+	scoreBoard(players);
 	
 	return 0;
 }
 
-void playGame(struct Player (*players)[], int *plyr, int *ball) {
+void playGame(struct Player *players, int *plyr, int *ball) {
 	int currentScore = 0;
 
     printf("Enter your score for ball#%d: ", ball + 1);
 	scanf("%d", &currentScore);
 	if(validateScore(currentScore)) {
 		players[plyr]->ballScores[ball] = currentScore;
+		players[plyr]->totalScore += currentScore;
 	}
 }
 
@@ -42,10 +45,16 @@ bool validateScore(int *currentScore) {
 	}
 }
 
-findWinner() {
-	
+void scoreBoard(struct Player *players) {
+	for(int i = 0; i < totPlyr;i++) {
+		printf("\n\t\tPlayer-%d: %s\n", i + 1, players->playerName);
+		for(int j = 0; j < 12; j++) {
+			printf(" Ball#%d: %d\n", j + 1, players->ballScores[j]);
+		}
+		printf("\n\tTotal Scores: %d", players->totalScore);
+	}
 }
 
-scoreBoard() {
+findWinner() {
 	
 }
